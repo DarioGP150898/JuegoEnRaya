9803626e6285cf4720075e526a19dd6eeb12ec6d
DESKTOP-7QUBBDD\RODRIGO <rodridelto@gmail.com>
2019-10-14 23:16:32 -0400

    segunda corrección

--- JuegoEnRaya/Form1.Designer.cs
+++ JuegoEnRaya/Form1.Designer.cs
@@ -54,7 +54,8 @@
             this.ayudaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(450, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(338, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,19 +65,20 @@
             this.nuevoJuegoToolStripMenuItem,
             this.salirToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 24);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
             this.toolStripMenuItem1.Text = "File";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // nuevoJuegoToolStripMenuItem
             // 
             this.nuevoJuegoToolStripMenuItem.Name = "nuevoJuegoToolStripMenuItem";
-            this.nuevoJuegoToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.nuevoJuegoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.nuevoJuegoToolStripMenuItem.Text = "Nuevo Juego";
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
@@ -85,22 +87,23 @@
             this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aCercaDeToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
             // aCercaDeToolStripMenuItem
             // 
             this.aCercaDeToolStripMenuItem.Name = "aCercaDeToolStripMenuItem";
-            this.aCercaDeToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.aCercaDeToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.aCercaDeToolStripMenuItem.Text = "A cerca de...";
             this.aCercaDeToolStripMenuItem.Click += new System.EventHandler(this.aCercaDeToolStripMenuItem_Click);
             // 
             // A1
             // 
             this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A1.Location = new System.Drawing.Point(12, 52);
+            this.A1.Location = new System.Drawing.Point(9, 42);
+            this.A1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.A1.Name = "A1";
-            this.A1.Size = new System.Drawing.Size(75, 75);
+            this.A1.Size = new System.Drawing.Size(56, 61);
             this.A1.TabIndex = 1;
             this.A1.UseVisualStyleBackColor = true;
             this.A1.Click += new System.EventHandler(this.button_click);
@@ -108,9 +111,10 @@
             // A2
             // 
             this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A2.Location = new System.Drawing.Point(93, 52);
+            this.A2.Location = new System.Drawing.Point(70, 42);
+            this.A2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.A2.Name = "A2";
-            this.A2.Size = new System.Drawing.Size(75, 75);
+            this.A2.Size = new System.Drawing.Size(56, 61);
             this.A2.TabIndex = 2;
             this.A2.UseVisualStyleBackColor = true;
             this.A2.Click += new System.EventHandler(this.button_click);
@@ -118,9 +122,10 @@
             // A3
             // 
             this.A3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.A3.Location = new System.Drawing.Point(174, 52);
+            this.A3.Location = new System.Drawing.Point(130, 42);
+            this.A3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.A3.Name = "A3";
-            this.A3.Size = new System.Drawing.Size(75, 75);
+            this.A3.Size = new System.Drawing.Size(56, 61);
             this.A3.TabIndex = 3;
             this.A3.UseVisualStyleBackColor = true;
             this.A3.Click += new System.EventHandler(this.button_click);
@@ -128,9 +133,10 @@
             // B3
             // 
             this.B3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B3.Location = new System.Drawing.Point(174, 143);
+            this.B3.Location = new System.Drawing.Point(130, 116);
+            this.B3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.B3.Name = "B3";
-            this.B3.Size = new System.Drawing.Size(75, 75);
+            this.B3.Size = new System.Drawing.Size(56, 61);
             this.B3.TabIndex = 6;
             this.B3.UseVisualStyleBackColor = true;
             this.B3.Click += new System.EventHandler(this.button_click);
@@ -138,9 +144,10 @@
             // B2
             // 
             this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B2.Location = new System.Drawing.Point(93, 143);
+            this.B2.Location = new System.Drawing.Point(70, 116);
+            this.B2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.B2.Name = "B2";
-            this.B2.Size = new System.Drawing.Size(75, 75);
+            this.B2.Size = new System.Drawing.Size(56, 61);
             this.B2.TabIndex = 5;
             this.B2.UseVisualStyleBackColor = true;
             this.B2.Click += new System.EventHandler(this.button_click);
@@ -148,9 +155,10 @@
             // B1
             // 
             this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.B1.Location = new System.Drawing.Point(12, 143);
+            this.B1.Location = new System.Drawing.Point(9, 116);
+            this.B1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.B1.Name = "B1";
-            this.B1.Size = new System.Drawing.Size(75, 75);
+            this.B1.Size = new System.Drawing.Size(56, 61);
             this.B1.TabIndex = 4;
             this.B1.UseVisualStyleBackColor = true;
             this.B1.Click += new System.EventHandler(this.button_click);
@@ -158,9 +166,10 @@
             // C3
             // 
             this.C3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C3.Location = new System.Drawing.Point(174, 238);
+            this.C3.Location = new System.Drawing.Point(130, 193);
+            this.C3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.C3.Name = "C3";
-            this.C3.Size = new System.Drawing.Size(75, 75);
+            this.C3.Size = new System.Drawing.Size(56, 61);
             this.C3.TabIndex = 9;
             this.C3.UseVisualStyleBackColor = true;
             this.C3.Click += new System.EventHandler(this.button_click);
@@ -168,9 +177,10 @@
             // C2
             // 
             this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C2.Location = new System.Drawing.Point(93, 238);
+            this.C2.Location = new System.Drawing.Point(70, 193);
+            this.C2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.C2.Name = "C2";
-            this.C2.Size = new System.Drawing.Size(75, 75);
+            this.C2.Size = new System.Drawing.Size(56, 61);
             this.C2.TabIndex = 8;
             this.C2.UseVisualStyleBackColor = true;
             this.C2.Click += new System.EventHandler(this.button_click);
@@ -178,18 +188,19 @@
             // C1
             // 
             this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.C1.Location = new System.Drawing.Point(12, 238);
+            this.C1.Location = new System.Drawing.Point(9, 193);
+            this.C1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.C1.Name = "C1";
-            this.C1.Size = new System.Drawing.Size(75, 75);
+            this.C1.Size = new System.Drawing.Size(56, 61);
             this.C1.TabIndex = 7;
             this.C1.UseVisualStyleBackColor = true;
             this.C1.Click += new System.EventHandler(this.button_click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 394);
+            this.ClientSize = new System.Drawing.Size(338, 320);
             this.Controls.Add(this.C3);
             this.Controls.Add(this.C2);
             this.Controls.Add(this.C1);
@@ -201,6 +212,7 @@
             this.Controls.Add(this.A1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
